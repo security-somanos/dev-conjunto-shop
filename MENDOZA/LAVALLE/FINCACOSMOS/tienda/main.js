b8bfebe352188502b914cd812a643db5e4d371aa
hatechamp <hatechamp@gmail.com>
2019-08-06 15:52:57 -0700

    firebase url edit

--- MENDOZA/LAVALLE/FINCACOSMOS/tienda/main.js
+++ MENDOZA/LAVALLE/FINCACOSMOS/tienda/main.js
@@ -113,7 +113,7 @@ var app = new Vue({
                 }
 
                 var self = this;
-                database.ref('salesMayorista/' + today).push(sale, function (error) {
+                database.ref('fincaCosmos/' + today).push(sale, function (error) {
                     if (error) {
                         console.log(error)
                     } else {
@@ -121,7 +121,7 @@ var app = new Vue({
                     }
                 });
 
-                database.ref('salesMayoristaArchive/' + today).push(sale, function (error) {
+                database.ref('fincaCosmosArchive/' + today).push(sale, function (error) {
                     if (error) {
                         console.log(error)
                     } else {
