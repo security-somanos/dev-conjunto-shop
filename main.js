11f89f82c53ae714672112cf30523a710938d943
hatechamp <hatechamp@gmail.com>
2019-12-04 03:02:03 -0800

    add style bb

---
+++ main.js
@@ -0,0 +1,11 @@
+const darkscreen = document.getElementById("darkscreen");
+
+var openModal = function(){
+    darkscreen.classList.remove("hide");
+}
+
+var closeModal = function(){
+    darkscreen.classList.add("hide");
+}
+
+
