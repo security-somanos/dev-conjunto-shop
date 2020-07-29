543bb63e3a45e7b4006600989d5f6127206cd4c7
cristo-fractal <fincacosmos>
2020-07-28 23:16:03 -0300

    ''

--- main.js
+++ main.js
@@ -8,4 +8,8 @@ var closeModal = function(){
     darkscreen.classList.add("hide");
 }
 
-
+async function getNodes() {
+    let response = await fetch ('https://elconjunto-backend.herokuapp.com/api/nodes/all')
+    let data = await response.json()
+    return data;
+}
